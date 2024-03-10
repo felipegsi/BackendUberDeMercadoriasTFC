034bee0ba7272374abbdf78591aba9c21f923cd8
Felipe <felipeguimaraes9787@gmail.com>
2024-03-10 23:23:01 +0000

    getOrderHistory almost ready

--- src/main/java/com/project/uber/model/User.java
+++ src/main/java/com/project/uber/model/User.java
@@ -33,6 +33,7 @@ public abstract class User implements UserDetails {
     private String city;
     @Column(nullable = false, length = 20)
     private int postalCode;
+
     //@Transient //apenas fica salvo no frontend a imagem
     //private byte[] profileImage;
 
